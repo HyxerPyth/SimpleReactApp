efd7c0a72b1a4b615a6893201b9cfed3d73906a0
Roman Loboda <hyawey2003@gmail.com>
2023-12-15 13:00:18 -0500

    Initial commit
    New Commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import Header from "./components/Header.jsx";
+import Nav from "./components/Nav.jsx";
+import Profile from "./components/Profile.jsx";
+
+const App = () => {
+  return (
+    <div className="app-wrapper">
+        <Header />
+        <Nav />
+        <Profile />
+    </div>
+  );
+}
+
+
+
+export default App;
